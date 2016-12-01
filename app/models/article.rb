d46e4caa59ca234b88738eea7389809600f11b43
Carlos Ricardo MS <carlosricardo42@gmail.com>
2016-12-01 17:45:46 -0600

    front end changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   validates :body, length: { maximum: 3000, minimum: 50 }, presence: true
+  validates :title, presence: true
   default_scope -> { order(created_at: :desc) }
 
   def can_edit(user)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+     get "signup", to: "devise/registrations#new"
+     get "login", to: "devise/sessions#new"
+     get "logout", to: "devise/sessions#destroy"
+  end
   get  '/home', to: 'static_pages#home'
   get  '/help', to: 'static_pages#help'
   resources :articles
