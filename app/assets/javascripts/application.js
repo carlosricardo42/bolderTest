2ee5a3cfc2ec890ca8beb30c9e533945bff72eae
Carlos Ricardo MS <carlosricardo42@gmail.com>
2016-12-02 10:21:10 -0600

    Fixing Assets to production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
 //= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
 //= require_tree .
